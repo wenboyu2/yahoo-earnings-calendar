f1e34836dc3c4bd892c1fdc9729ba017937f8f01
Wenbo Yu <24713139+wenboyu2@users.noreply.github.com>
2019-04-07 01:31:00 -0700

    fix: return more than 100 events on a day

--- yahoo_earnings_calendar/scraper.py
+++ yahoo_earnings_calendar/scraper.py
@@ -11,6 +11,7 @@ BASE_URL = 'https://finance.yahoo.com/calendar/earnings'
 BASE_STOCK_URL = 'https://finance.yahoo.com/quote'
 RATE_LIMIT = 2000.0
 SLEEP_BETWEEN_REQUESTS_S = 60 * 60 / RATE_LIMIT
+OFFSET_STEP = 100
 
 # Logging config
 logger = logging.getLogger()
@@ -27,7 +28,7 @@ class YahooEarningsCalendar(object):
     This is the class for fetching earnings data from Yahoo! Finance
     """
 
-    def __init__(self, delay = SLEEP_BETWEEN_REQUESTS_S):
+    def __init__(self, delay=SLEEP_BETWEEN_REQUESTS_S):
         self.delay = delay
 
     def _get_data_dict(self, url):
@@ -55,10 +56,12 @@ class YahooEarningsCalendar(object):
         except:
             raise Exception('Invalid Symbol or Unavailable Earnings Date')
 
-    def earnings_on(self, date):
+    def earnings_on(self, date, offset=0, count=1):
         """Gets earnings calendar data from Yahoo! on a specific date.
         Args:
             date: A datetime.date instance representing the date of earnings data to be fetched.
+            offset: Position to fetch earnings data from.
+            count: Total count of earnings on date.
         Returns:
             An array of earnings calendar data on date given. E.g.,
             [
@@ -77,14 +80,26 @@ class YahooEarningsCalendar(object):
         Raises:
             TypeError: When date is not a datetime.date object.
         """
+        if offset >= count:
+            return []
+
         if not isinstance(date, datetime.date):
             raise TypeError(
                 'Date should be a datetime.date object')
         date_str = date.strftime('%Y-%m-%d')
         logger.debug('Fetching earnings data for %s', date_str)
-        dated_url = '{0}?day={1}'.format(BASE_URL, date_str)
+        dated_url = '{0}?day={1}&offset={2}&size={3}'.format(
+            BASE_URL, date_str, offset, OFFSET_STEP)
         page_data_dict = self._get_data_dict(dated_url)
-        return page_data_dict['context']['dispatcher']['stores']['ScreenerResultsStore']['results']['rows']
+        stores_dict = page_data_dict['context']['dispatcher']['stores']
+        earnings_count = stores_dict['ScreenerCriteriaStore']['meta']['total']
+
+        # Recursively fetch more earnings on this date
+        new_offset = offset + OFFSET_STEP
+        more_earnings = self.earnings_on(date, new_offset, earnings_count)
+        curr_offset_earnings = stores_dict['ScreenerResultsStore']['results']['rows']
+
+        return curr_offset_earnings + more_earnings
 
     def earnings_between(self, from_date, to_date):
         """Gets earnings calendar data from Yahoo! in a date range.
@@ -126,11 +141,11 @@ class YahooEarningsCalendar(object):
         return earnings_data
 
 
-if __name__ == '__main__': # pragma: no cover
+if __name__ == '__main__':  # pragma: no cover
     date_from = datetime.datetime.strptime(
-        'May 5 2017  10:00AM', '%b %d %Y %I:%M%p')
+        'Feb 1 2018  10:00AM', '%b %d %Y %I:%M%p')
     date_to = datetime.datetime.strptime(
-        'May 8 2017  1:00PM', '%b %d %Y %I:%M%p')
+        'Feb 4 2018  1:00PM', '%b %d %Y %I:%M%p')
     yec = YahooEarningsCalendar()
     print(yec.earnings_on(date_from))
     print(yec.earnings_between(date_from, date_to))
